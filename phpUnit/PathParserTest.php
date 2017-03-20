8400a03c7facd70895b2f2e469e7c26e2f18c4ac
Matthew "Juniper" Barlett <emeraldinspirations+asusrog+1610@gmail.com>
2017-03-20 03:02:06 -0500

    Add Temporary Parser
    Verifying the validity of a path is really outside the scope of the
    project at this time.  In the future this class will be replaced with
    one that parses out the elements of the path and verifies that proper
    characters are used relative to the current operating system.

---
+++ phpUnit/PathParserTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Container for PathParser unit tests
+ *
+ * PHP Version 7
+ *
+ * @category  Library
+ * @package   Lib-filesystemabstraction
+ * @author    Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @copyright 2017 Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @license   TBD ../LICENSE.md
+ * @link      https://github.com/emeraldinspirations/lib-filesystemabstraction
+ */
+
+namespace emeraldinspirations\library\fileSystemAbstraction;
+
+/**
+ * Unit tests for PathParser
+ *
+ * @category  Library
+ * @package   Lib-filesystemabstraction
+ * @author    Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @copyright 2017 Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @license   TBD ../LICENSE.md
+ * @version   GIT: $Id$ In Development.
+ * @link      https://github.com/emeraldinspirations/lib-filesystemabstraction
+ */
+class PathParserTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Verifies object exists
+     *
+     * @return void
+     */
+    public function testValidPath()
+    {
+        $this->assertTrue(
+            PathParser::validatePath(''),
+            'Fails if class doesn\'t exist'
+        );
+    }
+
+}
---
+++ src/PathParser.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Container for PathParser unit tests
+ *
+ * PHP Version 7
+ *
+ * @category  Library
+ * @package   Lib-filesystemabstraction
+ * @author    Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @copyright 2017 Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @license   TBD ../LICENSE.md
+ * @link      https://github.com/emeraldinspirations/lib-filesystemabstraction
+ */
+
+namespace emeraldinspirations\library\fileSystemAbstraction;
+
+/**
+ * Unit tests for PathParser
+ *
+ * @category  Library
+ * @package   Lib-filesystemabstraction
+ * @author    Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @copyright 2017 Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @license   TBD ../LICENSE.md
+ * @version   GIT: $Id$ In Development.
+ * @link      https://github.com/emeraldinspirations/lib-filesystemabstraction
+ */
+class PathParser
+{
+
+    /**
+     * In the future this function will verify that the path passed is valid
+     * for the respective oporating system.
+     *
+     * @param string $Path The path to validate
+     *
+     * @return bool Path is valid
+     */
+    public function validatePath(string $Path)
+    {
+        //TODO: Add path validation code
+        return true;
+    }
+}
